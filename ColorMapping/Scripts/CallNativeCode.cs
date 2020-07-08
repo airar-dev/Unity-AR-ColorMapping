16dc4b5b7d58c20bfd7fdc3fb9ad2fee28ca4aa1
sung hoon,oh <oh@airar.co>
2020-07-08 16:31:17 +0900

    first Commit
     - OpenCV Android Bridge Test

---
+++ ColorMapping/Scripts/CallNativeCode.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class CallNativeCode : MonoBehaviour
+{
+    [DllImport("OpenCVBridge")]
+    private static extern float MyOpenCVTestMethod();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        // This Line should display "Foopluginmethod: 10"
+        GUI.Label(new Rect(15, 125, 450, 100), "MyOpenCVTestMethod: " + MyOpenCVTestMethod());
+
+    }
+}
