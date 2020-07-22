73e6397359cf3b07a037b906429b592e7ab690ee
sung hoon,oh <oh@airar.co>
2020-07-22 16:55:23 +0900

    이미지 최적화 작업중

--- ColorMapping/Scripts/CallNativeCode.cs
+++ ColorMapping/Scripts/CallNativeCode.cs
@@ -10,43 +10,43 @@ public class CallNativeCode : MonoBehaviour
     private static extern float MyOpenCVTestMethod();
 
 
-    //[DllImport("OpenCVBridge")]
-    //private static extern IntPtr GetWarpTexture(IntPtr myTexture, double[] src, double[] dst, int height, int width);
+    [DllImport("AirarColorMap")]
+    private static extern IntPtr GetWarpTexture(IntPtr myTexture, double[] src, double[] dst, int height, int width);
 
-    //public Texture2D m_Texture;
-    //public Renderer m_Renderer;
-    //private Texture2D testTex;
+    public Texture2D m_Texture;
+    public Renderer m_Renderer;
+    private Texture2D testTex;
 
-    //protected Color32[] m_Pixels;
-    //protected GCHandle m_PixelsHandle;
-    //protected IntPtr m_pixelPtr;
-    //protected IntPtr m_tempPtr;
+    protected Color32[] m_Pixels;
+    protected GCHandle m_PixelsHandle;
+    protected IntPtr m_pixelPtr;
+    protected IntPtr m_tempPtr;
 
     //7월 13일 검증 완료된 코드
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    //--------------------------------------------------------------------------------------//
-    //    double[] src = new double[] { 98, 70, 414, 190, 109, 386, 433, 370 };
-    //    double[] dst = new double[] { 0, 0, 512, 0, 0, 512, 512, 512 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        //--------------------------------------------------------------------------------------//
+        double[] src = new double[] { 98, 70, 414, 190, 109, 386, 433, 370 };
+        double[] dst = new double[] { 0, 0, 512, 0, 0, 512, 512, 512 };
 
-    //    //--------------------------------------------------------------------------------------//
+        //--------------------------------------------------------------------------------------//
 
-    //    m_Pixels = m_Texture.GetPixels32();
-    //    m_PixelsHandle = GCHandle.Alloc(m_Pixels, GCHandleType.Pinned);
-    //    m_pixelPtr = m_PixelsHandle.AddrOfPinnedObject();
+        m_Pixels = m_Texture.GetPixels32();
+        m_PixelsHandle = GCHandle.Alloc(m_Pixels, GCHandleType.Pinned);
+        m_pixelPtr = m_PixelsHandle.AddrOfPinnedObject();
 
 
-    //    m_tempPtr = GetWarpTexture(m_pixelPtr, src, dst, m_Texture.height, m_Texture.width);
+        m_tempPtr = GetWarpTexture(m_pixelPtr, src, dst, m_Texture.height, m_Texture.width);
 
-    //    Texture2D myTe = new Texture2D(m_Texture.width, m_Texture.height, TextureFormat.RGBA32, false);
-    //    myTe.LoadRawTextureData(m_tempPtr, myTe.height * myTe.width * 4);
-    //    myTe.Apply();
+        Texture2D myTe = new Texture2D(m_Texture.width, m_Texture.height, TextureFormat.RGBA32, false);
+        myTe.LoadRawTextureData(m_tempPtr, myTe.height * myTe.width * 4);
+        myTe.Apply();
 
-    //    File.WriteAllBytes(Application.persistentDataPath + "/test2.jpg", myTe.EncodeToJPG());
-    //    m_Renderer.material.mainTexture = myTe;
-    //}
-    //7월 13일 검증 완료된 코드 여기까지 
+        File.WriteAllBytes(Application.persistentDataPath + "/test2.jpg", myTe.EncodeToJPG());
+        m_Renderer.material.mainTexture = myTe;
+    }
+    //7월 13일 검증 완료된 코드 여기까지
 
 
     //unsafe void TextureToCVMat(Texture2D texData)
